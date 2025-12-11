cfd51ee913acdf833b280e07f00515b1bd320031
GaluhRR25 <grahayu251@gmail.com>
2025-12-11 15:41:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,8 @@ import streamlit as st
 st.write("Fighting, *Semuanya!* :sunglasses:")
 
 import streamlit as st
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] ")
-
-import streamlit as st
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+number = int(st.number_input("Insert a number",min_value=None, max_value=None,))
+if number %2=1:
+    st.write("Bilngan",number,"termasuk bilangan ganjil")
+else:
+    st.write("Bilngan",number,"termasuk bilangan genap")
