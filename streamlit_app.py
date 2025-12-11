b907c1016bac079ba2e56f52356e7940b6550de4
GaluhRR25 <grahayu251@gmail.com>
2025-12-11 15:27:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ st.title("🎈 Selamat Datang, Selamat menghitung")
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
 
 import streamlit as st
-st.write("Hello, *World!* :sunglasses:")
+st.write("Fighting, *Semuanya!* :sunglasses:")
