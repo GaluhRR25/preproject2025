69ab8784e90e144d5d3f769adf61136ef057afa2
GaluhRR25 <grahayu251@gmail.com>
2026-01-05 08:55:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,3 @@
-'''
-import streamlit as st
-st.title("🎈 Selamat Datang, Selamat menghitung")
-st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
-
-import streamlit as st
-st.write("Fighting, *Semuanya!* :sunglasses:")
-
-import streamlit as st
-number =(st.number_input("Insert a number",min_value=None, max_value=None,))
-if number%2==1:
-    st.write("Bilngan",number,"termasuk bilangan ganjil")
-else:
-    st.write("Bilngan",number,"termasuk bilangan genap")
-'''
     
 import streamlit as st
 
